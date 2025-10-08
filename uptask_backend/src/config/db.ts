a7834648b506754945b8000f0a6b6e61667e47f8
JPalacio3 <jpalaciocano3@gmail.com>
2025-10-07 18:14:35 -0600

    Agregar conexión a la base de datos y configuración de dotenv

---
+++ uptask_backend/src/config/db.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import colors from "colors";
+import { exit } from "node:process";
+
+export const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.DATABASE_URL);
+    console.log(
+      colors.bgBlue.bold(
+        `MongoDB conectado en: ${colors.magenta.bold(
+          connection.connection.host
+        )} : ${colors.magenta.bold(connection.connection.port.toString())}`
+      )
+    );
+  } catch (error) {
+    console.log(
+      colors.red.bold(`Error de conexión a la Base de Datos : ${error.message}`)
+    );
+    exit(1);
+  }
+};
